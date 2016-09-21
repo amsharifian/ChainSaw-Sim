84526e02fa902be71e3d0af9e3bda2bbfb95b15e
amiralis <am.sharifian@gmail.com>
2016-09-20 22:43:22 -0700

    Fix csh commands and change them to bash

--- gems-lib/scripts/calc_host.sh
+++ gems-lib/scripts/calc_host.sh
@@ -1,38 +1,40 @@
-#!/bin/csh -f
+#!/bin/bash -f
 
-set OS=`uname -s`
-set HOST_ARCH=`uname -m`
+export OS=`uname -s`
+export HOST_ARCH=`uname -m`
 
-switch ($OS)
-	case Linux:
-		set OS_PART=linux
-		breaksw
-	case SunOS:
-		set OS_PART=sol8-64
-		breaksw
-	case OSF1:
-		set OS_PART=tru64-gcc
-		breaksw
-	default:
-                set OS_PART=`echo $OS | sed 's/ /-/g'`
-endsw
+case ${OS} in
+    "Linux")
+		export OS_PART=linux
+        ;;
+    "SunOS")
+		export OS_PART=sol8-64
+        ;;
+    "OSF1")
+		export OS_PART=tru64-gcc
+        ;;
+    *)
+        export OS_PART=`echo $OS | sed 's/ /-/g'`
+        ;;
+esac;
 
-switch ($HOST_ARCH)
-	case i586:
-		set ARCH=x86
-		breaksw
-	case i686:
-		set ARCH=x86
-		breaksw
-	case x86_64:
-		set ARCH=amd64
-		breaksw
-	case sun4u:
-		set ARCH=v9
-		breaksw
-	default:
-                set ARCH=`echo $HOST_ARCH | sed 's/ /-/g'`
-endsw
+case ${HOST_ARCH} in
+    "i586")
+		export ARCH=x86
+        ;;
+    "i686")
+		export ARCH=x86
+        ;;
+    "x86_64")
+		export ARCH=amd64
+        ;;
+    "sun4u")
+		export ARCH=v9
+        ;;
+    *)
+        export ARCH=`echo $HOST_ARCH | sed 's/ /-/g'`
+        ;;
+esac;
     
 echo $ARCH-$OS_PART
 
